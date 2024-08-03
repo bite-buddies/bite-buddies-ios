57537f761ebe382fc9585b14e932e8ec0b287e06
Mercen <mercen@mercen.net>
2024-08-03 12:02:26 -0700

    Initial Commit

---
+++ BiteBuddies/BiteBuddiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiteBuddiesApp.swift
+//  BiteBuddies
+//
+//  Created by Mercen on 8/3/24.
+//
+
+import SwiftUI
+
+@main
+struct BiteBuddiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
